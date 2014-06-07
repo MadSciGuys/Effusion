e09257fee278681b1f43d91efecf3d82bd95eb93
Travis Whitaker <twhitak@its.jnj.com>
2014-06-07 02:49:19 +0000

    Un-genericized functions, added Jaccard difference

--- Effusion/Inference.hs
+++ Effusion/Inference.hs
@@ -1,13 +1,15 @@
 module Effusion.Inference (
     -- * List/String Analytics
     levenshtein
-   ,relativeLevenshtein
-   ,lexPerms
+   ,jaccard
+   ,normalLevenshtein
+   -- * Lexicographic Utility Functions
+   ,lexPermutations
    ,lexPairs
 ) where
 
 import Data.Array (range, listArray, (!))
-import Data.List  (genericLength, genericTake, permutations, subsequences)
+import Data.List  (length, genericTake, intersect, union, permutations, subsequences)
 
 -- | Compute the Levenshtein distance between two lists. Informally, the Levenshtein distance
 -- between two lists of equatable elements is the minimum number of sigle-element changes
@@ -26,10 +28,10 @@ import Data.List  (genericLength, genericTake, permutations, subsequences)
 --
 -- However, this naive implementation carries an exponential time complexity. 'levenshtein' instead
 -- uses a lazy immutable 'Array' to cache calls to @d@, allowing for /O(mn)/ time complexity.
-levenshtein :: Eq a => [a] -> [a] -> Integer
+levenshtein :: Eq a => [a] -> [a] -> Int
 levenshtein a b = d m n
-    where m = genericLength a
-          n = genericLength b
+    where m = length a
+          n = length b
 
           -- Transform input to 1-indexed array:
           a' = listArray (1,m) a
@@ -47,22 +49,38 @@ levenshtein a b = d m n
                                    ,ds ! (i, j-1)   + 1   -- Insertion
                                    ,ds ! (i-1, j-1) + 1 ] -- Substitution
 
--- | Compute the relative Levenshtein distance between two lists. This is 'levenshtein' divided by
--- the length of the longer of the two input lists. This allows for the comparison of Levenshtein
--- distances among pairs of lists.
-relativeLevenshtein :: Eq a => [a] -> [a] -> Double
-relativeLevenshtein a b = lev / maxlen
-    where lev    = fromIntegral $ levenshtein a b
-          maxlen = fromIntegral $ maximum [integerLength a, integerLength b]
-          integerLength :: [a] -> Integer
-          integerLength = genericLength
+-- | Compute the Jaccard distance between two lists. The Jaccard Index of a pair of sets is
+-- defined as the size of the intersection of the sets divided by the size of the union of the
+-- sets. This implies:
+-- prop> 0 <= jaccard a b <= 1
+--
+-- This function returns the Jaccard distance, which is simply one minus the Jaccard Index. This
+-- provides a metric of string difference rather than similarity. The Jaccard distance is generally
+-- "less strict" than the 'levenshtein' distance, i.e. it returns lower values for plausibly
+-- related strings.
+jaccard :: Eq a => [a] -> [a] -> Double
+jaccard a b = 1 - (intersectionLength / unionLength)
+    where intersectionLength = fromIntegral $ length $ intersect a b
+          unionLength       = fromIntegral $ length $ union a b
 
--- | Compute @n@ lexicographical permutations of list of elements.
-lexPerms :: Integral a => a -> [b] -> [[b]]
-lexPerms n xs = genericTake n xs'
+-- | Compute the normalized Levenshtein distance between two lists. This is 'levenshtein' divided
+-- by the length of the longer of the two input lists. This allows for the meaningful comparison of
+-- Levenshtein distances among pairs of lists.
+--
+-- The normalized Levenshtein distance is generally "more strict" than the 'jaccard' distance, i.e.
+-- it returns greater values for plausibly related strings.
+normalLevenshtein :: Eq a => [a] -> [a] -> Double
+normalLevenshtein a b = lev / maxlen
+    where lev    = fromIntegral $ levenshtein a b
+          maxlen = fromIntegral $ maximum [length a, length b]
+
+-- | Compute @n@ lexicographical permutations of list of elements. 'cycle' is used if the input
+-- list is too short.
+lexPermutations :: Integral a => a -> [b] -> [[b]]
+lexPermutations n xs = genericTake n xs'
     where xs' = concatMap permutations (subsequences $ cycle xs)
 
 -- | Provide @n@ pairs of lexicographical string permutations. Useful for testing fuzzy string
 -- matches. Note that the length of the returned string ins @n^2@.
 lexPairs :: Integral a => a -> [b] -> [([b], [b])]
-lexPairs n xs = [(a, b) | a <- lexPerms n xs, b <- lexPerms n xs]
+lexPairs n xs = [(a, b) | a <- lexPermutations n xs, b <- lexPermutations n xs]
