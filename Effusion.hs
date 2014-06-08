977cd3ff12f4f2569fd489566560c966f36e8135
Travis Whitaker <twhitak@its.jnj.com>
2014-06-08 19:41:41 +0000

    Add a bit more structure to top level docs.

--- Effusion.hs
+++ Effusion.hs
@@ -38,11 +38,21 @@ module Effusion (
     -- * Data Modeling
     -- $datamodeling
 
+    -- ** Abstract Data Model
+    -- $abstractdatamodel
+
     ,module Effusion.Abstract
+
+    -- ** ISA88/ISA95 \"Recipe\" Data Model
+    -- $recipedatamodel
+
     ,module Effusion.Rx
 
     -- * Data Analysis
     -- $dataanalysis
+
+    -- ** Catamorphisms
+    -- $catamorphisms
 
     ,module Effusion.Catamorphism
 ) where
@@ -67,25 +77,36 @@ import Effusion.Catamorphism
 -- streaming output from an industrial control/monitoring system might be concave.
 
 -- $convexdata
--- The 'Effusion.Convex' module provides tools for handling densely interconnected data and is
+-- The "Effusion.Convex" module provides tools for handling densely interconnected data and is
 -- intended to be used in conjuction with a data persistence system supporting the RDF data
 -- interchange standard and SPARQL query language, although many components may be useful in any
--- system (namely those residing outside of the 'Effusion.Convex.SPARQL' and 'Effusion.Convex.RDF'
+-- system (namely those residing outside of the "Effusion.Convex.SPARQL" and "Effusion.Convex.RDF"
 -- modules). The focus of this module is on the construction, transformation, storage, and querying
 -- of graphs and common graph patterns.
 
 -- $concavedata
--- The 'Effusion.Concave' module provides tools for handling sparsely interconnected, regular data.
--- Tools to handle data of this nature as JSON and in conjunction with MongoDB are also provided (
--- as these are the tools used by the author), however an RDBMS or even a key/value store may be
--- just as effective in many environments. The focus of this module is on handling simple, regular
--- matrices.
+-- The "Effusion.Concave" module provides tools for handling sparsely interconnected, regular data.
+-- Tools to handle data of this nature as JSON and in conjunction with MongoDB are also provided
+-- (as these are the tools used by the author), however an RDBMS or even a key/value store may be
+-- just as effective in many environments. The focus of this module is on handling relationally
+-- simple tables, perhaps mergeable on only one or two record members.
 
 -- $inferencing
--- Something about record linking.
+-- The "Effusion.Inference" module provides generic tools for data integration, like merging data
+-- sets on shared identifiers, record linking, string analysis, and fuzzy string matching. Common
+-- data interchange structures like tables, trees, and graphs are the focus.
 
 -- $datamodeling
 -- Something about data modeling.
 
+-- $abstractdatamodel
+-- Something about abstract data model.
+
+-- $recipedatamodel
+-- Something about recipe data model.
+
 -- $dataanalysis
 -- Something about data analysis.
+
+-- $catamorphisms
+-- Something about catamorphisms.
