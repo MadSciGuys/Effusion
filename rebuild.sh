7fefd40f8f2982cd21d0f5d6da055c25bbf50ec3
Travis Whitaker <twhitak@its.jnj.com>
2014-06-09 11:51:01 +0000

    Add rebuild.sh

---
+++ rebuild.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cabal clean
+cabal configure
+cabal build
+cabal install
+cabal configure
+cabal haddock --hyperlink-sources
+cp -r dist/doc/html/Effusion/ /data/drupal_web/sites/default/files/codebase_docs/
+chmod --recursive 0755 /data/drupal_web/sites/default/files/codebase_docs/
